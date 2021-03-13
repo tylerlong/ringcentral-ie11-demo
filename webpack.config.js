51c67310089eb1fe3911980db5b3161725f02833
Tyler Long <tyler4long@gmail.com>
2021-03-12 16:26:39 -0800

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = {
   mode: 'development',
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    browsers: ['IE >= 11'],
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'RingCentral IE 11 Demo',
